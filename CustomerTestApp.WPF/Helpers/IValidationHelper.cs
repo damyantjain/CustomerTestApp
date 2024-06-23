2913cac431b297438773f5742ffede7c742e05d5
Damyant Jain <damyantjain@gmail.com>
2024-06-23 10:44:01 -0700

    Implemented INotifyDataErrorInfo for form validation.

---
+++ CustomerTestApp.WPF/Helpers/IValidationHelper.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel;
+
+namespace CustomerTestApp.WPF.Helpers
+{
+    /// <summary>
+    /// The Interface extending the INotifyDataErrorInfo and adding useful functionalities.
+    /// </summary>
+    public interface IValidationHelper : INotifyDataErrorInfo
+    {
+        /// <summary>
+        /// Clears all the error.
+        /// </summary>
+        /// <param name="PropertyName"></param>
+        void ClearErrors(string PropertyName);
+
+        /// <summary>
+        /// Add a new error.
+        /// </summary>
+        /// <param name="propertyName"></param>
+        /// <param name="errorMessage"></param>
+        void AddError(string propertyName, string errorMessage);
+    }
+}
--- CustomerTestApp.WPF/ViewModels/CustomerEditViewModel.cs
+++ CustomerTestApp.WPF/ViewModels/CustomerEditViewModel.cs
@@ -1,7 +1,10 @@
 ﻿using CommunityToolkit.Mvvm.Input;
 using CommunityToolkit.Mvvm.Messaging;
+using CustomerTestApp.WPF.Helpers;
 using CustomerTestApp.WPF.Messages;
 using CustomerTestApp.WPF.Models;
+using System.Collections;
+using System.ComponentModel;
 
 namespace CustomerTestApp.WPF.ViewModels
 {
@@ -14,17 +17,27 @@ namespace CustomerTestApp.WPF.ViewModels
 
         private Customer _editableCustomer;
 
+        private readonly ValidationHelper _validationHelper;
+
+        private string _firstName;
+
+        private string _lastName;
+
+        private string _email;
+
+        private int _discount;
+
         #endregion
 
         #region Public Properties
 
         /// <summary>
-        /// The save customer command saves the selected customer.
+        /// The save customer command.
         /// </summary>
         public IRelayCommand SaveCustomerCommand { get; }
 
         /// <summary>
-        /// The Selected Customer
+        /// The customer object for editing.
         /// </summary>
         public Customer EditableCustomer
         {
@@ -33,23 +46,126 @@ namespace CustomerTestApp.WPF.ViewModels
             {
                 _editableCustomer = value;
                 OnPropertyChanged(nameof(EditableCustomer));
+                UpdateCustomerProperties();
             }
         }
+
+        /// <summary>
+        /// The Id of the customer.
+        /// </summary>
+        public int Id => EditableCustomer?.Id ?? 0;
+
+        /// <summary>
+        /// The first name of the Customer.
+        /// </summary>
+        public string FirstName
+        {
+            get => _firstName;
+            set
+            {
+                _firstName = value;
+                _validationHelper.ClearErrors(nameof(FirstName));
+                if (string.IsNullOrWhiteSpace(_firstName))
+                {
+                    _validationHelper.AddError(nameof(FirstName), "First Name is required");
+                }
+                OnPropertyChanged(nameof(FirstName));
+            }
+        }
+
+        /// <summary>
+        /// The last name of the customer
+        /// </summary>
+        public string LastName
+        {
+            get => _lastName;
+            set
+            {
+                _lastName = value;
+                _validationHelper.ClearErrors(nameof(LastName));
+                if (string.IsNullOrWhiteSpace(_lastName))
+                {
+                    _validationHelper.AddError(nameof(LastName), "Last Name is required");
+                }
+                OnPropertyChanged(nameof(LastName));
+            }
+        }
+
+        /// <summary>
+        /// The Email of the customer.
+        /// </summary>
+        public string Email
+        {
+            get => _email;
+            set
+            {
+                _email = value;
+                _validationHelper.ClearErrors(nameof(Email));
+                if (string.IsNullOrWhiteSpace(_email))
+                {
+                    _validationHelper.AddError(nameof(Email), "Email is required");
+                }
+                OnPropertyChanged(nameof(Email));
+            }
+        }
+
+        /// <summary>
+        /// The discount of the customer.
+        /// </summary>
+        public int Discount
+        {
+            get => _discount;
+            set
+            {
+                _discount = value;
+                _validationHelper.ClearErrors(nameof(Discount));
+                if (_discount < 0m || _discount > 30m)
+                {
+                    _validationHelper.AddError(nameof(Discount), "Discount must be between 0 and 30");
+                }
+                OnPropertyChanged(nameof(Discount));
+            }
+        }
+
+        /// <summary>
+        /// Boolean to determine if the user data can be saved
+        /// </summary>
+        public bool CanSave => !HasErrors;
+
+        /// <summary>
+        /// Boolean to determine if there are any errors with validation.
+        /// </summary>
+        public bool HasErrors => _validationHelper.HasErrors;
+
+        /// <summary>
+        /// Event when Error are changed.
+        /// </summary>
+        public event EventHandler<DataErrorsChangedEventArgs> ErrorsChanged;
 
         #endregion
 
         #region Public Methods
 
-        /// <summary>
-        /// The CustomerEditViewModel constructor register the Messenger.
-        /// </summary>
         public CustomerEditViewModel()
         {
             SaveCustomerCommand = new RelayCommand(SaveCustomer);
+            _validationHelper = new ValidationHelper();
+            _validationHelper.ErrorsChanged += GetValidationHelper_ErrorChanged;
             WeakReferenceMessenger.Default.Register<SelectedCustomerChangedMessage>(this, (r, m) =>
             {
-                EditableCustomer = m.SelectedCustomer.Clone();
+                EditableCustomer = m.SelectedCustomer?.Clone();
             });
+        }
+
+        private void GetValidationHelper_ErrorChanged(object sender, DataErrorsChangedEventArgs e)
+        {
+            ErrorsChanged?.Invoke(this, e);
+            OnPropertyChanged(nameof(CanSave));
+        }
+
+        public IEnumerable GetErrors(string? propertyName)
+        {
+            return _validationHelper.GetErrors(propertyName);
         }
 
         #endregion
@@ -60,9 +176,29 @@ namespace CustomerTestApp.WPF.ViewModels
         {
             if (EditableCustomer != null)
             {
+                EditableCustomer.FirstName = FirstName;
+                EditableCustomer.LastName = LastName;
+                EditableCustomer.Email = Email;
+                EditableCustomer.Discount = Discount;
+
                 WeakReferenceMessenger.Default.Send(new SaveCustomerMessage(EditableCustomer));
             }
         }
+
+        private void UpdateCustomerProperties()
+        {
+            if (EditableCustomer != null)
+            {
+                FirstName = EditableCustomer.FirstName;
+                LastName = EditableCustomer.LastName;
+                Email = EditableCustomer.Email;
+                Discount = EditableCustomer.Discount;
+            }
+        }
+
+        #endregion
+
+        #region Public Methods
 
         #endregion
     }
